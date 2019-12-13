35cc78d12d379c44c8257d842851728f61752da5
Willem De Roover <willem.deroover@aca-it.be>
2019-12-13 19:59:02 +0100

    Day 13 2019

--- src/day12/Puzzle12.java
+++ src/day12/Puzzle12.java
@@ -9,33 +9,33 @@ import java.util.List;
 public class Puzzle12 {
 
 	public static void main(String[] args) {
-		List<Pair> xAxis = createAxis(-7, -12, 6, 4);
-		List<Pair> yAxis = createAxis(-8, -3, -17, -10);
-		List<Pair> zAxis = createAxis(9,-4,-9,-6);
+		List<Coordinate> xAxis = createAxis(-7, -12, 6, 4);
+		List<Coordinate> yAxis = createAxis(-8, -3, -17, -10);
+		List<Coordinate> zAxis = createAxis(9,-4,-9,-6);
 
 		calculateEnergyOfSystem(xAxis, yAxis, zAxis);
-		calculateStepsForFullCycle(xAxis, yAxis, zAxis);
+		calculateStepsForCycle(xAxis, yAxis, zAxis);
 	}
 
-	private static List<Pair> createAxis(int... axis) {
-		return Arrays.stream(axis).mapToObj(Pair::new).collect(toList());
+	private static List<Coordinate> createAxis(int... axis) {
+		return Arrays.stream(axis).mapToObj(Coordinate::new).collect(toList());
 	}
 
-	private static void calculateEnergyOfSystem(List<Pair> xAxis, List<Pair> yAxis, List<Pair> zAxis) {
+	private static void calculateEnergyOfSystem(List<Coordinate> xAxis, List<Coordinate> yAxis, List<Coordinate> zAxis) {
 		for (int i = 1; i <= 1000; i++) {
-			moveOneCycle(xAxis);
-			moveOneCycle(yAxis);
-			moveOneCycle(zAxis);
+			moveOneStep(xAxis);
+			moveOneStep(yAxis);
+			moveOneStep(zAxis);
 		}
 		System.out.println("The total energy of the system after 1000 cycles: " + calculateEnergy(xAxis, yAxis, zAxis));
 	}
 
-	private static void moveOneCycle(List<Pair> axis) {
+	private static void moveOneStep(List<Coordinate> axis) {
 		axis.forEach(anAxis -> axis.stream().filter(otherAxis -> !otherAxis.equals(anAxis)).forEach(anAxis::recalculateVelocity));
-		axis.forEach(Pair::applyVelocity);
+		axis.forEach(Coordinate::applyVelocity);
 	}
 
-	private static int calculateEnergy(List<Pair> xAxis, List<Pair> yAxis, List<Pair> zAxis) {
+	private static int calculateEnergy(List<Coordinate> xAxis, List<Coordinate> yAxis, List<Coordinate> zAxis) {
 		int energy = 0;
 		for(int i = 0; i < xAxis.size(); i++) {
 			energy += calculateEnergy(xAxis.get(i), yAxis.get(i), zAxis.get(i));
@@ -43,20 +43,20 @@ public class Puzzle12 {
 		return energy;
 	}
 
-	private static int calculateEnergy(Pair x, Pair y, Pair z) {
+	private static int calculateEnergy(Coordinate x, Coordinate y, Coordinate z) {
 		return (Math.abs(x.getPosition()) + Math.abs(y.getPosition()) + Math.abs(z.getPosition())) * (Math.abs(x.getVelocity()) + Math.abs(y.getVelocity()) + Math.abs(z.getVelocity()));
 	}
 
-	private static void calculateStepsForFullCycle(List<Pair> xAxis, List<Pair> yAxis, List<Pair> zAxis) {
-		BigInteger stepsOfCompleteCycle = lcd(lcd(calculateStepsForFullCycle(xAxis), calculateStepsForFullCycle(yAxis)), calculateStepsForFullCycle(zAxis));
-		System.out.println("it took " + stepsOfCompleteCycle + " to complete a full cycle");
+	private static void calculateStepsForCycle(List<Coordinate> xAxis, List<Coordinate> yAxis, List<Coordinate> zAxis) {
+		BigInteger stepsForCycle = lcd(lcd(calculateStepsForCycle(xAxis), calculateStepsForCycle(yAxis)), calculateStepsForCycle(zAxis));
+		System.out.println("it took " + stepsForCycle + " to complete a full cycle");
 	}
 
-	private static BigInteger calculateStepsForFullCycle(List<Pair> axis) {
-		List<Pair> origin = clone(axis);
+	private static BigInteger calculateStepsForCycle(List<Coordinate> axis) {
+		List<Coordinate> origin = clone(axis);
 		int cycleNumber = 0;
 		while(true) {
-			moveOneCycle(axis);
+			moveOneStep(axis);
 			cycleNumber++;
 			if(isSame(axis, origin)) {
 				return new BigInteger(String.valueOf(cycleNumber));
@@ -64,11 +64,11 @@ public class Puzzle12 {
 		}
 	}
 
-	private static List<Pair> clone(List<Pair> axis) {
-		return axis.stream().map(Pair::new).collect(toList());
+	private static List<Coordinate> clone(List<Coordinate> axis) {
+		return axis.stream().map(Coordinate::new).collect(toList());
 	}
 
-	private static boolean isSame(List<Pair> axis, List<Pair> otherAxis) {
+	private static boolean isSame(List<Coordinate> axis, List<Coordinate> otherAxis) {
 		for(int i = 0; i < axis.size(); i++) {
 			if(!axis.get(i).isSame(otherAxis.get(i))) {
 				return false;
@@ -85,17 +85,17 @@ public class Puzzle12 {
 
 }
 
-class Pair {
+class Coordinate {
 	private int position;
 	private int velocity = 0;
 
-	Pair(int position) {
+	Coordinate(int position) {
 		this.position = position;
 	}
 
-	Pair(Pair pair) {
-		this.position = pair.position;
-		this.velocity = pair.velocity;
+	Coordinate(Coordinate coordinate) {
+		this.position = coordinate.position;
+		this.velocity = coordinate.velocity;
 	}
 
 	int getPosition() {
@@ -110,7 +110,7 @@ class Pair {
 		this.position += velocity;
 	}
 
-	void recalculateVelocity(Pair other) {
+	void recalculateVelocity(Coordinate other) {
 		if (other.position > this.getPosition()) {
 			velocity++;
 		} else if (other.position < this.getPosition()) {
@@ -118,8 +118,8 @@ class Pair {
 		}
 	}
 
-	boolean isSame(Pair pair) {
-		return position == pair.position && velocity == pair.velocity;
+	boolean isSame(Coordinate coordinate) {
+		return position == coordinate.position && velocity == coordinate.velocity;
 	}
 
 }
