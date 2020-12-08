b8816b626599b2f1d9e2ac382ff082c90f59da2d
Willem De Roover <willem.deroover@hotmail.com>
2020-12-08 08:00:31 +0100

    day08

--- src/_2020/day07/Puzzle.java
+++ src/_2020/day07/Puzzle.java
@@ -12,54 +12,54 @@ import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Puzzle {
+	public class Puzzle {
 
-	private static Map<String, String> bagMap;
+		private static Map<String, String> bagMap;
 
-	public static void main(String[] args) throws IOException {
-		bagMap = Files.lines(Paths.get("src/_2020/day07/input.txt"))
-				.collect(toMap(Puzzle::parentBag, Function.identity()));
+		public static void main(String[] args) throws IOException {
+			bagMap = Files.lines(Paths.get("src/_2020/day07/input.txt"))
+					.collect(toMap(Puzzle::parentBag, Function.identity()));
 
-		System.out.println(containsShinyGold());
-		System.out.println(countChildBags("shiny gold"));
-	}
+			System.out.println(containsShinyGold());
+			System.out.println(countChildBags("shiny gold"));
+		}
 
-	private static String parentBag(String s) {
-		Matcher matcher = Pattern.compile("^(\\w+\\s\\w+)").matcher(s);
-		matcher.find();
-		return matcher.group(0);
-	}
+		private static String parentBag(String s) {
+			Matcher matcher = Pattern.compile("^(\\w+\\s\\w+)").matcher(s);
+			matcher.find();
+			return matcher.group(0);
+		}
 
-	private static long containsShinyGold() {
-		return bagMap.keySet()
-				.stream()
-				.filter(Puzzle::containsShinyGold)
-				.count();
-	}
+		private static long containsShinyGold() {
+			return bagMap.keySet()
+					.stream()
+					.filter(Puzzle::containsShinyGold)
+					.count();
+		}
 
-	private static boolean containsShinyGold(String s) {
-		String content = bagMap.get(s);
-		Matcher matcher = Pattern.compile("(\\d)\\s(\\w+\\s\\w+)").matcher(content);
-		List<String> childBags = new ArrayList<>();
-		while(matcher.find()) {
-			if (matcher.group(2).equals("shiny gold")) {
-				return true;
+		private static boolean containsShinyGold(String s) {
+			String content = bagMap.get(s);
+			Matcher matcher = Pattern.compile("(\\d)\\s(\\w+\\s\\w+)").matcher(content);
+			List<String> childBags = new ArrayList<>();
+			while(matcher.find()) {
+				if (matcher.group(2).equals("shiny gold")) {
+					return true;
+				}
+				childBags.add(matcher.group(2));
 			}
-			childBags.add(matcher.group(2));
+			return childBags.stream().anyMatch(Puzzle::containsShinyGold);
 		}
-		return childBags.stream().anyMatch(Puzzle::containsShinyGold);
-	}
 
-	private static int countChildBags(String startBag) {
-		String content = bagMap.get(startBag);
-		Matcher matcher = Pattern.compile("(\\d)\\s(\\w+\\s\\w+)").matcher(content);
-		int count = 0;
-		while (matcher.find()) {
-			int amount =  Integer.parseInt(matcher.group(1));
-			String bagName = matcher.group(2);
-			count += amount + (amount * countChildBags(bagName));
+		private static int countChildBags(String startBag) {
+			String content = bagMap.get(startBag);
+			Matcher matcher = Pattern.compile("(\\d)\\s(\\w+\\s\\w+)").matcher(content);
+			int count = 0;
+			while (matcher.find()) {
+				int amount =  Integer.parseInt(matcher.group(1));
+				String bagName = matcher.group(2);
+				count += amount + (amount * countChildBags(bagName));
+			}
+			return count;
 		}
-		return count;
-	}
 
-}
+	}
--- src/_2020/day08/Puzzle.java
+++ src/_2020/day08/Puzzle.java
@@ -3,12 +3,60 @@ package _2020.day08;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Puzzle {
 
 	public static void main(String[] args) throws IOException {
-		Files.lines(Paths.get("src/_2020/day08/input.txt"));
-		
+		List<String> operations = Files.lines(Paths.get("src/_2020/day08/input.txt"))
+				.collect(Collectors.toList());
+
+//		runOperations(operations); first part
+
+		for(int i = 0; i < operations.size(); i++) {
+			List<String> modifiedList = new ArrayList<>(operations);
+			String operation = modifiedList.get(i);
+			if(operation.startsWith("nop")) {
+				modifiedList.set(i, operation.replace("nop", "jmp"));
+				runOperations(modifiedList);
+			} else if (operation.startsWith("jmp")) {
+				modifiedList.set(i, operation.replace("jmp", "nop"));
+				runOperations(modifiedList);
+			}
+		}
 	}
 
+	private static void runOperations(List<String> operations) {
+		int accumulator = 0;
+		int position = 0;
+		Set<Integer> visited = new HashSet<>();
+
+		while (!visited.contains(position) && position < operations.size()) {
+			visited.add(position);
+			String operation = operations.get(position);
+			String instruction = operation.substring(0, 3);
+			String amount = operation.substring(4);
+
+			switch(instruction) {
+				case "acc":
+					accumulator += Integer.parseInt(amount);
+					position++;
+					break;
+				case "jmp":
+					position += Integer.parseInt(amount);
+					break;
+				case "nop":
+					position++;
+					break;
+				}
+		}
+
+		if(position == operations.size()) { //remove for first part;
+			System.out.println(accumulator);
+		}
+	}
 }
